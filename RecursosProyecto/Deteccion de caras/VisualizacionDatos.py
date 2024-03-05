aa6ca8e0d8cf2ff26962bdde84e535c339fff241
Marcos Ballesteros <marcos.ballesterosdiaz@riberadeltajo.es>
2024-03-05 21:21:02 +0100

    Muestra de imagenes con sus puntos faciales

--- RecursosProyecto/Deteccion de caras/VisualizacionDatos.py
+++ RecursosProyecto/Deteccion de caras/VisualizacionDatos.py
@@ -7,24 +7,42 @@ df = pd.read_csv('../datasets_proyectos/puntos_faciales/data.csv')
 
 # Itera sobre las filas del DataFrame
 for index, row in df.iterrows():
-    # Obtiene las coordenadas x e y del punto
-    x = row['left_eye_center_x']
-    y = row['left_eye_center_y']
+    # Obtiene las coordenadas de los puntos de interés
+    points = [
+        ('left_eye_center_x', 'left_eye_center_y'),
+        ('right_eye_center_x', 'right_eye_center_y'),
+        ('left_eye_inner_corner_x', 'left_eye_inner_corner_y'),
+        ('left_eye_outer_corner_x', 'left_eye_outer_corner_y'),
+        ('right_eye_inner_corner_x', 'right_eye_inner_corner_y'),
+        ('right_eye_outer_corner_x', 'right_eye_outer_corner_y'),
+        ('left_eyebrow_inner_end_x', 'left_eyebrow_inner_end_y'),
+        ('left_eyebrow_outer_end_x', 'left_eyebrow_outer_end_y'),
+        ('right_eyebrow_inner_end_x', 'right_eyebrow_inner_end_y'),
+        ('right_eyebrow_outer_end_x', 'right_eyebrow_outer_end_y'),
+        ('nose_tip_x', 'nose_tip_y'),
+        ('mouth_left_corner_x', 'mouth_left_corner_y'),
+        ('mouth_right_corner_x', 'mouth_right_corner_y'),
+        ('mouth_center_top_lip_x', 'mouth_center_top_lip_y'),
+        ('mouth_center_bottom_lip_x', 'mouth_center_bottom_lip_y')
+    ]
 
     # Obtiene la representación de la imagen y la convierte en un array de valores
     image_values = np.fromstring(row['Image'], sep=' ', dtype=int)
 
-    # Realiza el reshape de la imagen a 94x94
-    image_array = image_values.reshape(96, 96)  # Cambié 94 a 96 porque el reshape es de 96x96
+    # Realiza el reshape de la imagen a 96x96
+    image_array = image_values.reshape(96, 96)
 
     # Muestra la imagen
     plt.imshow(image_array, cmap='gray')
 
-    # Agrega un punto rojo en las coordenadas
-    plt.scatter(x, y, color='red', marker='o')
+    # Agrega puntos rojos en las coordenadas
+    for x_col, y_col in points:
+        x = row[x_col]
+        y = row[y_col]
+        plt.scatter(x, y, color='red', marker='o')
 
     # Ajusta el aspecto de la imagen
     plt.axis('off')
 
-    # Muestra la imagen con el punto rojo
+    # Muestra la imagen con los puntos rojos
     plt.show()
